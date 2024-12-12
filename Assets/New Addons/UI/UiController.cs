e122d2480120d7a2e7122f5f65292e5ccf812a3c
gtmills905 <gtmills905@gmail.com>
2024-12-12 18:30:33 +0000

    idk

--- Assets/New Addons/UI/UiController.cs
+++ Assets/New Addons/UI/UiController.cs
@@ -11,6 +11,13 @@ public class UiController : MonoBehaviour
     public TMP_Text deathText;
 
 
+
+    public GameObject birdUI; // Bird-specific UI elements
+    public GameObject farmerUI; // Farmer-specific UI elements
+
+
+    public GameObject universalUI;
+
     public Slider healthSlider;
     public static UiController instance;
 
